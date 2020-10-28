9c104c6174d9e19cd7171bd41b7d73a12f262924
Cristian Gómez <cristian.gomez@zentagroup.com>
2020-10-28 09:09:41 -0300

    Restart all

--- front-end/src/app/core/models/model.ts
+++
@@ -1,7 +0,0 @@
-export interface Friend {
-    id: number
-    name: string
-    gender: string
-    createdAt: Date
-    updatedAt: Date
-}
--- front-end/src/app/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-import { animate, style, transition, trigger } from '@angular/animations';
-import { BASE } from 'src/environments/environment';
-
-
-export const friends = BASE.api + 'friends'
