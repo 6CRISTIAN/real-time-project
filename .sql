4c6c25c04ae4c8baa701d319933b024a36ed7e63
Cristian Gómez <cristian.gomez@zentagroup.com>
2020-10-28 17:04:33 -0300

    Added scripts on the .sql file to create table and insert first record on psql db

--- .sql
+++ .sql
@@ -1,3 +1,22 @@
+CREATE TABLE public.my_friends
+(
+    id integer NOT NULL DEFAULT nextval('my_friends_id_seq'::regclass),
+    name character varying(60) COLLATE pg_catalog."default" NOT NULL,
+    gender character varying(1) COLLATE pg_catalog."default" NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT my_friends_pkey PRIMARY KEY (id)
+)
+
+
+
+INSERT INTO public.my_friends(
+	name, gender, "createdAt", "updatedAt")
+	VALUES ('Sebastián', 'M', '2020-10-28 17:03:00.86-03', '2020-10-28 17:03:00.86-03');
+
+
+
+
 CREATE OR REPLACE FUNCTION notify_updated() RETURNS TRIGGER AS $$
 DECLARE
 	updated_values jsonb;
@@ -18,6 +37,7 @@ BEGIN
 	RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
+
 
 
 DROP TRIGGER IF EXISTS on_updated_friend ON my_friends;
